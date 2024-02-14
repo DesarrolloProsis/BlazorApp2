051b28571865cbb50120134628be327dc12ab804
KarenC Martinez <prosis.kmartinez@gmail.com>
2024-02-13 18:03:53 -0600

    Servicio e interfaz en cliente

---
+++ Client/Pages/FetchData.razor.cs
@@ -0,0 +1,40 @@
+﻿using BlazorApp2.Client.Interfaces;
+using BlazorApp2.Client.Services;
+using BlazorApp2.Shared;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Logging;
+
+
+namespace BlazorApp2.Client.Pages
+{
+    public partial class FetchData
+    {
+        //[Inject]
+        //private IWeatherForecastCService _weatherForecastCService { get; set; }
+        //private IEnumerable<WeatherForecast?> _forecasts;
+        //private WeatherForecast[] _forecast;
+
+
+        //public FetchData(IWeatherForecastCService WeatherForecastCService)
+        //{
+        //    _weatherForecastCService = WeatherForecastCService;
+        //}
+
+        //protected override async Task OnInitializedAsync()
+        //{
+        //    try
+        //    {
+        //        _forecasts = await _weatherForecastCService.GetForecastAsync();
+        //        //forecasts = await Http.GetFromJsonAsync<WeatherForecast[]>("WeatherForecast");
+        //        //return _forecasts;
+
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Console.WriteLine(ex.Message);
+
+        //    }
+
+        //}
+    }
+}
