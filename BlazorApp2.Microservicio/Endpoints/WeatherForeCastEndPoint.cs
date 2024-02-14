886cee560eddc18e8e665ddc61e924be64308234
Eliasim Mendoza <eliasimmzsa@gmail.com>
2024-02-13 18:01:45 -0600

    Sernvicio, interfaz y Endpoint en microservicio

---
+++ BlazorApp2.Microservicio/Endpoints/WeatherForeCastEndPoint.cs
@@ -0,0 +1,29 @@
+﻿using BlazorApp2.Microservicio.Interfaces;
+
+namespace BlazorApp2.Microservicio.Endpoints
+{
+    public static class WeatherForeCastEndPoint
+    {
+        public static void MapWeatherForecastEndPoints(this IEndpointRouteBuilder routes)
+        {
+            routes.MapGet("/weatherforecast", async (IWeatherForecastService weatherForecast) =>
+            {
+                try
+                {
+                    var response = await weatherForecast.GetWeatherForecast();
+                    return Results.Ok(response);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                    return Results.Problem(ex.Message);
+                }
+            })
+            .WithName("Weatherforecast")
+            .Produces<IResult>(StatusCodes.Status200OK, "application/pdf")
+            .Produces<HttpValidationProblemDetails>(StatusCodes.Status400BadRequest, "application/problem+json")
+            .Produces<HttpValidationProblemDetails>(StatusCodes.Status500InternalServerError,
+                "application/problem+json"); ;
+        }
+    }
+}
---
+++ BlazorApp2.Microservicio/Services/WeatherForecastService.cs
@@ -0,0 +1,34 @@
+﻿using BlazorApp2.Microservicio.Interfaces;
+using BlazorApp2.Shared;
+
+namespace BlazorApp2.Microservicio.Services
+{
+    public class WeatherForecastService : IWeatherForecastService
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        public async Task<IEnumerable<WeatherForecast>> GetWeatherForecast()
+        {
+            try
+            {
+                var forecast = Enumerable.Range(1, 5).Select(index =>
+                new WeatherForecast
+                {
+                    Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    TemperatureC = Random.Shared.Next(-20, 55),
+                    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                })
+            .ToArray();
+                return forecast;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+    }
+}
